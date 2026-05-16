e5d186b659d8b7c9fc5542079b876f0e530ea2c9
RA1-Asur <123820449+RA1-Asur@users.noreply.github.com>
2026-05-16 08:11:32 +0530

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -5,5 +5,15 @@
     width="40"
     height="40"
     style="object-fit: contain;"
+    class="logo-img"
   />
 </template>
+
+<style scoped>
+.logo-img {
+  filter: brightness(1);
+}
+.dark .logo-img {
+  filter: brightness(0) invert(1);
+}
+</style>
