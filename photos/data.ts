2b3548727a0c6ae765007c37ff21720c1c792217
RA1-Asur <123820449+RA1-Asur@users.noreply.github.com>
2026-05-18 11:04:44 +0530

    Update data.ts

--- photos/data.ts
+++ photos/data.ts
@@ -1,42 +1,13 @@
-export interface PhotoMate {
-  text?: string
-  lang?: string
-  blurhash?: string
-}
-
-export interface Photo extends PhotoMate {
+export interface Photo {
   name: string
   url: string
+  text?: string
 }
 
-const metaInfo = Object.entries(
-  import.meta.glob<PhotoMate>('./**/*.json', {
-    eager: true,
-    import: 'default',
-  }),
-).map(([name, data]) => {
-  name = name.replace(/\.\w+$/, '').replace(/^\.\//, '')
-  return {
-    name,
-    data,
-  }
-})
-
-const photos = Object.entries(
-  import.meta.glob<string>('./**/*.{jpg,png,JPG,PNG}', {
-    eager: true,
-    query: '?url',
-    import: 'default',
-  }),
-)
-  .map(([name, url]): Photo => {
-    name = name.replace(/\.\w+$/, '').replace(/^\.\//, '')
-    return {
-      ...metaInfo.find(info => info.name === name)?.data,
-      name,
-      url,
-    }
-  })
-  .sort((a, b) => b.name.localeCompare(a.name))
+const photos: Photo[] = [
+  // Your photos will appear here!
+  // Example:
+  // { name: 'my-photo', url: 'https://i.imgur.com/XXXXX.jpg', text: 'Caption here' },
+]
 
 export default photos
