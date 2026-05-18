a7307092b86e89d8e793d401f6709e86ad18f5ac
RA1-Asur <123820449+RA1-Asur@users.noreply.github.com>
2026-05-18 11:09:06 +0530

    Update data.ts

--- photos/data.ts
+++ photos/data.ts
@@ -2,12 +2,10 @@ export interface Photo {
   name: string
   url: string
   text?: string
+  link?: string
 }
 
 const photos: Photo[] = [
-  // Your photos will appear here!
-  // Example:
-  // { name: 'my-photo', url: 'https://i.imgur.com/XXXXX.jpg', text: 'Caption here' },
 ]
 
 export default photos
