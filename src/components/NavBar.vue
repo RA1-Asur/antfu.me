97d07c1a588982d1707946d15a21f4e691800c79
RA1-Asur <123820449+RA1-Asur@users.noreply.github.com>
2026-05-21 11:06:00 +0530

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -34,37 +34,17 @@ const { y: scroll } = useWindowScroll()
           <span class="lt-md:hidden">Blog</span>
           <div i-ri-article-line md:hidden />
         </RouterLink>
-        <RouterLink to="/projects" title="Projects">
-          <span class="lt-md:hidden">Projects</span>
-          <div i-ri-lightbulb-line class="md:hidden" />
-        </RouterLink>
-        <RouterLink to="/talks" class="lt-md:hidden" title="Talks">
-          Talks
-        </RouterLink>
-        <RouterLink to="/sponsors-list" title="Sponsors">
-          <span class="lt-md:hidden">Sponsors</span>
-          <div i-ri-heart-line class="md:hidden" />
-        </RouterLink>
-        <RouterLink to="/podcasts" class="lt-md:hidden" title="Podcasts">
-          <div i-ri-mic-line />
-        </RouterLink>
         <RouterLink to="/photos" title="Photos">
           <div i-ri-camera-3-line />
         </RouterLink>
-        <RouterLink to="/demos" title="Demos">
-          <div i-ri-screenshot-line />
-        </RouterLink>
-        <!-- <RouterLink to="/chat" title="Let's Chat">
-          <div i-ri-chat-1-line />
-        </RouterLink> -->
-        <a href="https://bsky.app/profile/antfu.me" target="_blank" title="Bluesky" class="lt-md:hidden">
-          <div i-ri-bluesky-line />
+        <a href="https://youtube.com/@yaha_bhi_athens" target="_blank" title="YouTube">
+          <div i-simple-icons-youtube />
         </a>
-        <a href="https://github.com/antfu" target="_blank" title="GitHub" class="lt-md:hidden">
+        <a href="https://instagram.com/axat.moments" target="_blank" title="Instagram" class="lt-md:hidden">
+          <div i-simple-icons-instagram />
+        </a>
+        <a href="https://github.com/RA1-Asur" target="_blank" title="GitHub" class="lt-md:hidden">
           <div i-uil-github-alt />
-        </a>
-        <a href="/feed.xml" target="_blank" title="RSS" class="lt-md:hidden">
-          <div i-la-rss-square style="font-size:1.25rem; margin: 0 -0.125rem;" />
         </a>
         <ToggleTheme />
       </div>
@@ -76,13 +56,11 @@ const { y: scroll } = useWindowScroll()
 .header h1 {
   margin-bottom: 0;
 }
-
 .logo {
   position: absolute;
   top: 1.5rem;
   left: 1.5rem;
 }
-
 .nav {
   padding: 2rem;
   width: 100%;
@@ -90,15 +68,12 @@ const { y: scroll } = useWindowScroll()
   grid-template-columns: auto max-content;
   box-sizing: border-box;
 }
-
 .nav > * {
   margin: auto;
 }
-
 .nav img {
   margin-bottom: 0;
 }
-
 .nav a {
   cursor: pointer;
   text-decoration: none;
@@ -107,18 +82,15 @@ const { y: scroll } = useWindowScroll()
   opacity: 0.6;
   outline: none;
 }
-
 .nav a:hover {
   opacity: 1;
   text-decoration-color: inherit;
 }
-
 .nav .right {
   display: grid;
   grid-gap: 1.2rem;
   grid-auto-flow: column;
 }
-
 .nav .right > * {
   margin: auto;
 }
