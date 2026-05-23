0616bd4f11a763c16f364e4d33fa3b717f0259e8
RA1-Asur <123820449+RA1-Asur@users.noreply.github.com>
2026-05-23 09:32:13 +0530

    photos: add "Sab Bhaad me Jaaye"

--- photos/data.ts
+++ photos/data.ts
@@ -6,6 +6,7 @@ export interface Photo {
 }
 
 const photos: Photo[] = [
+  { name: 'Sab Bhaad me Jaaye', url: 'https://i.postimg.cc/FsL9fMr5/IMG-20241014-032736.jpg', text: '#3 lectures', link: 'https://i.postimg.cc/FsL9fMr5/IMG-20241014-032736.jpg' },
   { name: 'Comeback toh hoga', url: 'https://i.imgur.com/UGCBKZQ.gif', text: '#calllofduty' },
 ]
 
