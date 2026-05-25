fa3e909011d25c9f2ee47ada173bf573bfca2c72
RA1-Asur <123820449+RA1-Asur@users.noreply.github.com>
2026-05-25 10:38:04 +0530

    photos: add "You have to start from somewhere Akshat"

--- photos/data.ts
+++ photos/data.ts
@@ -6,6 +6,7 @@ export interface Photo {
 }
 
 const photos: Photo[] = [
+  { name: 'You have to start from somewhere Akshat', url: 'https://i.postimg.cc/Yq9c9wKL/images-(1)-2026-05-25T103408-455.jpg', link: 'https://www.reddit.com/r/wholesomememes/comments/w1486e/funny_but_wholesome/' },
   { name: 'Sab Bhaad me Jaaye', url: 'https://i.postimg.cc/FsL9fMr5/IMG-20241014-032736.jpg', text: '#3 lectures', link: 'https://i.postimg.cc/FsL9fMr5/IMG-20241014-032736.jpg' },
   { name: 'Comeback toh hoga', url: 'https://i.imgur.com/UGCBKZQ.gif', text: '#calllofduty' },
 ]
