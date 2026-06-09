f178431a15a5a72a9930b3be19f1f2314b3296c4
RA1-Asur <123820449+RA1-Asur@users.noreply.github.com>
2026-06-09 14:21:28 +0530

    photos: remove "Comeback toh hoga"

--- photos/data.ts
+++ photos/data.ts
@@ -7,7 +7,6 @@ export interface Photo {
 
 const photos: Photo[] = [
   { name: 'Sab Bhaad me Jaaye', url: 'https://i.postimg.cc/FsL9fMr5/IMG-20241014-032736.jpg', text: '#3 lectures', link: 'https://i.postimg.cc/FsL9fMr5/IMG-20241014-032736.jpg' },
-  { name: 'Comeback toh hoga', url: 'https://i.imgur.com/UGCBKZQ.gif', text: '#calllofduty' },
 ]
 
 export default photos
